f1f6fed80c10d6cf91589438214bf4c918ae8f47
AryanKumarSwain <aryankumarswain99@gmail.com>
2026-09-12 13:03:26 +0530

    chore: add allowed dev origins to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const __dirname = dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  allowedDevOrigins: ['timetablepro.webncode.in', '*.webncode.in'],
   typescript: {
     ignoreBuildErrors: true,
   },
